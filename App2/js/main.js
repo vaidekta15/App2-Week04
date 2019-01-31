8a8e752a8802f85b39106d890c9633122211cfd4
ekta15.vaid <ekta15.vaid@gmail.com>
2019-01-31 11:48:19 -0500

    i committed

--- App2/js/main.js
+++ App2/js/main.js
@@ -1,1 +1,10 @@
 ﻿// Your code here!
+function myFunction() {
+    var cat = new Date()
+    undefined
+    document.body.innerHTML = "<h1> The date is " + cat + "  </h1>  "
+    document.body.innerHTML = "<h1> The date is " +
+        (cat.getMonth() + 1) + " / " +
+        cat.getDate() + " / " +
+        cat.getFullYear() + " </h1> "
+}
